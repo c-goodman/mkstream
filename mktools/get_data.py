3ce192968db3b340d92410c5b6b9da620ad2e36b
Cooper Goodman <c-goodman@tamu.edu>
2024-07-16 22:45:32 -0400

    Option to forgo the usecols parameter

--- mktools/get_data.py
+++ mktools/get_data.py
@@ -2,13 +2,16 @@ import numpy as np
 import pandas as pd
 
 
-def load_data_pd(sheet_name: str, sheet_id: str, usecols: list[str]) -> pd.DataFrame:
+def load_data_pd(
+    sheet_name: str, sheet_id: str, usecols: list[str] | None = None
+) -> pd.DataFrame:
     """Load specified sheet from Google Drive.
 
     Args:
         sheet_name (str): Name of the table to be loaded
         sheet_id (str): Google Drive Sheets ID Unique value stored in `.env` file.
         Environment variable is accessible with `dotenv.load_dotenv()` and `os.environ["<YOUR_SHEET_ID>"]`.
+        usecols (list[str] | None, optional): Optional list of column headers to load. Defaults to None.
 
     Returns:
         pd.DataFrame: Pandas DataFrame loaded from the specified Google Drive sheet.
@@ -17,11 +20,19 @@ def load_data_pd(sheet_name: str, sheet_id: str, usecols: list[str]) -> pd.DataF
     # Construct the Google Drive URL using `sheet_id` and `sheet_name`
     url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
 
-    # Load the data from the URL as a Polars DataFrame
-    # Fill Null values with `np.nan`
-    df = pd.read_csv(
-        url,
-        usecols=usecols,
-    ).fillna(np.nan)
+    if isinstance(usecols, list):
+        # Load the data from the URL as a Polars DataFrame
+        # Fill Null values with `np.nan`
+        df = pd.read_csv(
+            url,
+            usecols=usecols,
+        ).fillna(np.nan)
+
+    if usecols is None:
+        # Load the data from the URL as a Polars DataFrame
+        # Fill Null values with `np.nan`
+        df = pd.read_csv(
+            url,
+        ).fillna(np.nan)
 
     return df
