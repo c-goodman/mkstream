d01e73ac6c675b2228fd650e99ff211ad6b02c7d
Cooper Goodman <c-goodman@tamu.edu>
2024-07-16 22:45:09 -0400

    Option to return the additional valid dataframe

--- mktools/validate_data.py
+++ mktools/validate_data.py
@@ -1,7 +1,9 @@
 import pandas as pd
 
 
-def validate_bad_uids(df: pd.DataFrame) -> None:
+def validate_bad_uids(
+    df: pd.DataFrame, return_valid: bool = False
+) -> pd.DataFrame | tuple[pd.DataFrame, pd.DataFrame]:
 
     msk = (df["UID"].value_counts().reset_index()["count"] > 4) | (
         df["UID"].value_counts().reset_index()["count"] < 2
@@ -10,5 +12,13 @@ def validate_bad_uids(df: pd.DataFrame) -> None:
     bad_uids = df[df["UID"].isin(df["UID"].value_counts().index[msk])].reset_index(
         drop=True
     )
+
+    if return_valid:
+
+        valid_uids = df[
+            ~df["UID"].isin(df["UID"].value_counts().index[msk])
+        ].reset_index(drop=True)
+
+        return bad_uids, valid_uids
 
     return bad_uids
